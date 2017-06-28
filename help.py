e2f253f47500746a9a1affc232d8079914a6a3df
andrey <andrey@gmail.com>
2017-06-28 18:05:25 +0300

    task 4

--- help.py
+++ help.py
@@ -4,6 +4,9 @@
 
 #https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D1%82%D0%B0-%D1%80%D0%B0%D1%81%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5
 
+
+#доверительные интервалы
+
 # s = u'Тест чтото там'
 # print s
 # print(s.decode('UTF-8'))
@@ -17,3 +20,12 @@ import pandas as pd
 
 np.linalg.matrix_rank([1,2,3])# rank matrix
 #scipy.optimize.anneal - mothod otziga
+
+
+'''
+https://www.youtube.com/watch?v=Z4Zy3WQBbhk
+https://www.youtube.com/watch?v=Qlco5dPrimU&list=PLqW3u411sCU6e7EtkcjouUvDGYGI3tci_
+https://www.youtube.com/watch?v=9SqYCWyvbys&index=35&list=PLqW3u411sCU6e7EtkcjouUvDGYGI3tci_
+
+p1 или p2 = p1 + p2
+'''
--- statistic/beta.py
+++ statistic/beta.py
@@ -1,30 +1,73 @@
+import math
 import scipy.stats as st
 from matplotlib import pylab as plt
 
 x_coordinates = []
 y_coordinates = []
 
-A = 5
-B = 1
+A = 5.
+B = 1.
 N = 10000
 
+
+t = (3**5)
+
+mathematical_expectation = A / (A + B)
+dispersion = A * B / (A + B)**2 * (A + B + 1)
+
+print "mathematical_expectation", mathematical_expectation
+print "dispersion", dispersion
 
 def BetaFn(x):
     return st.beta.pdf(x, A, B)
 
-for x in range(N):
-    x = float(x) / N
-    y = BetaFn(x)
-    # print "X = %s, Y = %s" % (x, y)
-    x_coordinates.append(x)
-    y_coordinates.append(y)
+def show_beta_func_graph():
+    for x in range(N):
+        x = float(x) / N
+        y = BetaFn(x)
+        # print "X = %s, Y = %s" % (x, y)
+        x_coordinates.append(x)
+        y_coordinates.append(y)
 
-fig = plt.figure()
-fig.suptitle('Beta Function', fontsize=20)
-plt.xlabel('N', fontsize=18)
-plt.ylabel('Value', fontsize=16)
+    fig = plt.figure()
+    fig.suptitle('Beta Function', fontsize=20)
+    plt.xlabel('N', fontsize=18)
+    plt.ylabel('Value', fontsize=16)
 
-stats = st.beta.rvs(A, B, size=N)
-plt.hist(stats, 100, normed=True, facecolor='green', alpha=0.5)
-plt.plot(x_coordinates, y_coordinates)
-plt.show()
+    stats = st.beta.rvs(A, B, size=N)
+    plt.hist(stats, 100, normed=True, facecolor='green', alpha=0.5)
+    plt.plot(x_coordinates, y_coordinates)
+    plt.show()
+
+    plt.clf()
+
+def get_beta_range(N):
+    return st.beta.rvs(A, B, size=N)
+
+def x_sample_mean(X):
+    return sum(X) / len(X)
+
+def show_CPT_gist(m, k=2):
+    values = []
+    plot_x = []
+
+    for i in range(m):
+        beta_range = get_beta_range(k)
+        X = x_sample_mean(beta_range)
+        values.append(X)
+        #plot_x.append(X - mathematical_expectation / (math.sqrt(float(dispersion)) / math.sqrt(float(k))))
+
+    plt.xlabel('count='+str(m) + ', n=' + str(k), fontsize=16)
+    #plt.plot(plot_x)
+    plt.hist(values, 70, normed=True, facecolor='green', alpha=0.5)
+    plt.show()
+    plt.clf()
+
+show_beta_func_graph()
+# show_CPT_gist(1000, 2)
+# show_CPT_gist(1000, 5)
+# show_CPT_gist(1000, 10)
+# show_CPT_gist(1000, 50)
+# show_CPT_gist(1000, 80)
+show_CPT_gist(1000, 200)
+show_CPT_gist(10000, 200)
