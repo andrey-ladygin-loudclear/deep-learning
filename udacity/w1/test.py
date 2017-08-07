a6c597d1779b4d2cdae3e7791e5727f78b0a975b
Srivoknovskiy-home <sri@gmail.com>
2017-08-07 22:39:47 +0300

    m7

---
+++ udacity/w1/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+s = np.array(5)
+
+print s.shape
+
+v = np.array([1,2,3,4])
+print v.reshape(4)
+
+values = [1,2,3,4,5]
+values = np.array(values) + 5
+
+
+np.multiply(m, n)
+m.T
