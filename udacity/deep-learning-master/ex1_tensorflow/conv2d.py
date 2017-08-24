be0899322e83ba63756ed7ab914d3f4213e29e51
andrey <andrey@gmail.com>
2017-08-24 16:55:09 +0300

    ex2

--- udacity/deep-learning-master/ex1_tensorflow/conv2d.py
+++ udacity/deep-learning-master/ex1_tensorflow/conv2d.py
@@ -1,6 +1,19 @@
+import tensorflow as tf
+
 input = tf.placeholder(tf.float32, (None, 32, 32, 3))
 filter_weights = tf.Variable(tf.truncated_normal((8, 8, 3, 20))) # (height, width, input_depth, output_depth)
 filter_bias = tf.Variable(tf.zeros(20))
 strides = [1, 2, 2, 1] # (batch, height, width, depth)
 padding = 'SAME'
 conv = tf.nn.conv2d(input, filter_weights, strides, padding) + filter_bias
+
+
+input = tf.placeholder(tf.float32, (None, 4, 4, 5))
+filter_shape = [1, 2, 2, 1]
+strides = [1, 2, 2, 1]
+padding = 'VALID'
+pool = tf.nn.max_pool(input, filter_shape, strides, padding)
+
+
+
+weight= tf.Variable(tf.truncated_normal([conv_ksize[0], conv_ksize[1], x_depth, conv_num_outputs],stddev=0.1))
