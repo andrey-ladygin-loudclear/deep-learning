a6690cb1406cd1d51cbee5ac79cdbae0763334ae
andrey <andrey@gmail.com>
2017-08-25 16:06:16 +0300

    ex2

--- udacity/deep-learning-master/ex1_tensorflow/conv2d.py
+++ udacity/deep-learning-master/ex1_tensorflow/conv2d.py
@@ -17,3 +17,13 @@ pool = tf.nn.max_pool(input, filter_shape, strides, padding)
 
 
 weight= tf.Variable(tf.truncated_normal([conv_ksize[0], conv_ksize[1], x_depth, conv_num_outputs],stddev=0.1))
+
+
+
+def conv2d(input):
+    # Filter (weights and bias)
+    F_W = tf.Variable(tf.truncated_normal((2, 2, 1, 3)))
+    F_b = tf.Variable(tf.zeros(3))
+    strides = [1, 2, 2, 1]
+    padding = 'VALID'
+    return tf.nn.conv2d(input, F_W, strides, padding) + F_b
