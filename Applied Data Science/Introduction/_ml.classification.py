cc5ba62d8ce339d9ef1d0450d409569bf8388403
Srivoknovskiy-home <sri@gmail.com>
2017-10-24 22:37:22 +0300

    kaggle

---
+++ Applied Data Science/Introduction/_ml.classification.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import  train_test_split
+
+fruits = pd.read_table('fruit_data_with_colors.txt')
+
+print(fruits.head())
+
+lookup_fruit_name = dict(zip(fruits.fruit_label.unique(), fruits.fruit_name.unique()))
+print(lookup_fruit_name)
+
+X = fruits[['mass', 'width', 'height']]
+y = fruits['fruit_label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+from matplotlib import cm
+cmap = cm.get_cmap('gnuplot')
+scatter = pd.scatter_matrix(X_train, c=y_train, marker='o', s=40, hist_kwds={'bins':15}, figsize=(12,12), cmap=cmap)
+
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X_train['width'], X_train['height'], X_train['mass'], c=y_train, marker='o', s=100)
+ax.set_xlabel('width')
+ax.set_ylabel('height')
+ax.set_zlabel('mass')
+plt.show()
+
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+knn.score(X_test, y_test)
+fruit_prediction = knn.predict([[20, 4.3, 5.5]])
+print(lookup_fruit_name[fruit_prediction[0]])
+from adspy_shared_utilities import plot_fruit_knn
+plot_fruit_knn(X_train, y_train, 5, 'uniform')
