050cb30c5bd44bb22b8c325436f5464f90abf0a3
Srivoknovskiy-home <sri@gmail.com>
2017-10-25 23:16:31 +0300

    kaggle

--- Applied Data Science/Introduction/_ml.classification.py
+++ Applied Data Science/Introduction/_ml.classification.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from sklearn.model_selection import  train_test_split
 
 fruits = pd.read_table('fruit_data_with_colors.txt')
